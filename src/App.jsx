5ba48834ceced1eb27487ee9ff4d02066118a355
Swapnil Lokam <swapnillokam29@gmail.com>
2025-07-12 12:29:08 +0530

    Inital necessitites

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Outlet } from "react-router-dom"
+import './App.css'
+import Navbar from "./components/Navbar"
+
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      {/*Outlet renders all the children of the app */}
+      <main className="min-h-screen w-1/2 mx-auto px-4 py-6 font-primary">
+        <Outlet />
+      </main>
+      <footer>Footer</footer>
+    </>
+  )
+}
+
+export default App
