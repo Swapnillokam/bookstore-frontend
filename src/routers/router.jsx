dd7106eeec46ebdabfb2bfe240d4c6adf8b21360
Swapnil Lokam <swapnillokam29@gmail.com>
2025-08-01 15:22:23 +0530

    checkout page created

--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -4,6 +4,7 @@ import Home from "../pages/home/Home";
 import Login from "../components/LOgin";
 import Register from "../components/Register";
 import CartPage from "../pages/books/CartPage";
+import CheckoutPage from "../pages/books/CheckoutPage";
 
 const router = createBrowserRouter([
     {
@@ -37,6 +38,10 @@ const router = createBrowserRouter([
             {
                 path: "/cart",
                 element: <CartPage />
+            },
+            {
+                path: "/checkout",
+                element: <CheckoutPage />
             },
         ],
     },
