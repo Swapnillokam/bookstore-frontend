a70f2ddf730f2e01523cedf71c332494ea549dca
Swapnil Lokam <swapnillokam29@gmail.com>
2025-09-13 19:44:21 +0530

    frontend outlet width manged

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,10 @@ function App() {
     <>
       <Navbar />
       {/*Outlet renders all the children of the app.jsx */}
-      <main className="min-h-screen w-1/2 mx-auto px-4 py-6 font-primary">
+      <main className="min-h-screen w-3/4 mx-auto px-4 py-6 font-primary">
         <Outlet />
       </main>
-      <footer><Footer/></footer>
+      <footer><Footer /></footer>
     </>
   )
 }
