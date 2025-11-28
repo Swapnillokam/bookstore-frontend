ba5f22530958d410f7f450b1d938fc98d67f98c6
Swapnil Lokam <swapnillokam29@gmail.com>
2025-11-28 12:16:44 +0530

    deployment issue for login component

--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/home/Home";
-import Login from "../components/LOgin";
 import Register from "../components/Register";
 import CartPage from "../pages/books/CartPage";
 import CheckoutPage from "../pages/books/CheckoutPage";
