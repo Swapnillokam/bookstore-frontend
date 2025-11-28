fa9f59b964b13c2db733a831627f689a465aa754
Swapnil Lokam <swapnillokam29@gmail.com>
2025-11-28 13:51:39 +0530

    modifed base url to deployed backend domain

--- src/utils/getBaseUrl.js
+++ src/utils/getBaseUrl.js
@@ -1,3 +1,6 @@
-const getBaseUrl = () => "http://localhost:5000"
+// const getBaseUrl = () => "http://localhost:5000"
+
+//modifying the base url with the deployed vercel backend domain
+const getBaseUrl = () => "https://bookstore-backend-7ze7.vercel.app"
 
 export default getBaseUrl;
